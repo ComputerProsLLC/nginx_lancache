ba378adde0599117604bada62f22e31e40e93db0
Chris Lovett <clovett@mcnc.org>
2024-01-04 03:23:05 +0000

    Added 5m TTL and restructured domain list

--- powershell/Create-CacheZones.ps1
+++ powershell/Create-CacheZones.ps1
@@ -1,4 +1,4 @@
- <#
+﻿<#
 .Description
 Sets or removes DNS Zones for caching in the DNS Service.
 
@@ -15,10 +15,23 @@ Param(
     [parameter(ParameterSetName="Cache",mandatory=$true)][IPAddress]$CacheIp
 )
 
-$microsoftZones = 'download.windowsupdate.com', 'tlu.dl.delivery.mp.microsoft.com',
-    'officecdn.microsoft.com', 'officecdn.microsoft.com.edgesuite.net'
-$googleZones = 'dl.google.com', 'gvt1.com'
-$adobeZones = 'ardownload.adobe.com', 'ccmdl.adobe.com', 'agsupdate.adobe.com'
+$microsoftZones = @(
+    [PSCustomObject]@{ Zone='download.windowsupdate.com'; Record='Both' },
+    [PSCustomObject]@{ Zone='tlu.dl.delivery.mp.microsoft.com'; Record='Both' },
+    [PSCustomObject]@{ Zone='officecdn.microsoft.com'; Record='Root' },
+    [PSCustomObject]@{ Zone='officecdn.microsoft.com.edgesuite.net'; Record='Root' }
+)
+
+$googleZones = @(
+    [PSCustomObject]@{ Zone='dl.google.com'; Record='Root' },
+    [PSCustomObject]@{ Zone='gvt1.com'; Record='WildCard' }
+)
+
+$adobeZones = @(
+    [PSCustomObject]@{ Zone='ardownload.adobe.com'; Record='Root' },
+    [PSCustomObject]@{ Zone='ccmdl.adobe.com'; Record='Root' },
+    [PSCustomObject]@{ Zone='agsupdate.adobe.com'; Record='Root' }
+)
 
 $zoneGroup = $microsoftZones + $googleZones + $adobeZones
 
@@ -34,7 +47,7 @@ function Set-CacheRecord {
             Remove-DnsServerResourceRecord -ZoneName $ZoneName -Name "*" -Confirm:$false
         }
         if ( !$record ) {
-            Add-DnsServerResourceRecord -A -IPv4Address $CacheIp -Name "*" -ZoneName $ZoneName
+            Add-DnsServerResourceRecord -A -IPv4Address $CacheIp -Name "*" -ZoneName $ZoneName -TimeToLive 00:05:00
         }
     }
     if ( $Root ) {
@@ -43,22 +56,22 @@ function Set-CacheRecord {
             Remove-DnsServerResourceRecord -ZoneName $ZoneName -Name "@" -Confirm:$false
         }
         if ( !$record ) {
-            Add-DnsServerResourceRecord -A -IPv4Address $CacheIp -Name "@" -ZoneName $ZoneName
+            Add-DnsServerResourceRecord -A -IPv4Address $CacheIp -Name "@" -ZoneName $ZoneName -TimeToLive 00:05:00
         }
     }
 }
 
 # Emergency shut off section removes all cache zones from DNS
 if ( $EmergencyShutOff ) {
-    $zoneGroup.ForEach{Remove-DnsServerZone -Name $_ -Force -Confirm:$false}
+    $zoneGroup.ForEach{Remove-DnsServerZone -Name $_.Zone -Force -Confirm:$false}
 }
 
 else {
     foreach ( $z in $zoneGroup ) {
         try {
-            $zone = Get-DnsServerZone -Name $z -ErrorAction Ignore
+            $zone = Get-DnsServerZone -Name $z.Zone -ErrorAction Ignore
             if ( !$zone ) {
-                Add-DnsServerPrimaryZone -Name $z -ReplicationScope Domain -DynamicUpdate None
+                Add-DnsServerPrimaryZone -Name $z.Zone -ReplicationScope Domain -DynamicUpdate None
             }
         }
         catch {
@@ -66,19 +79,28 @@ else {
             break
         }
 
-        switch -regex ( $z ) {
-            '(download.windowsupdate.com|tlu.dl.delivery.mp.microsoft.com)' {
+        switch ( $z.Record ) {
+            'Both' {
                 try {
-                    Set-CacheRecord -Wildcard -Root -ZoneName $z
+                    Set-CacheRecord -Wildcard -Root -ZoneName $z.Zone
                 }
                 catch {
                     $Error[0]
                     break
                 }
             }
-            'gvt1.com' {
+            'WildCard' {
                 try {
-                    Set-CacheRecord -Wildcard -ZoneName $z
+                    Set-CacheRecord -Wildcard -ZoneName $z.Zone
+                }
+                catch {
+                    $Error[0]
+                    break        
+                }
+            }
+            'Root' {
+                try {
+                    Set-CacheRecord -Root -ZoneName $z.Zone
                 }
                 catch {
                     $Error[0]
@@ -86,14 +108,8 @@ else {
                 }
             }
             default {
-                try {
-                    Set-CacheRecord -Root -ZoneName $z
-                }
-                catch {
-                    $Error[0]
-                    break        
-                }
+                Write-Host -ForegroundColor "Yellow" "$($z.Zone) 'Record' should be either 'WildCard', 'Root' or 'Both'"
             }
         }
     }
-} 
+}
